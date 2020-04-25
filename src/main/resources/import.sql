badfaa86008a6e766aee6fbf21f5eeea6c4d8ec8
vtrcavassana <victor.cavassana@outlook.com>
2020-04-24 21:54:13 -0300

    fixed id appearence on table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into previsao (umidade, dia_Semana, descr, temp_Max, temp_Min) values (82, 'Domingo', 'Previsão 1', 22.7, 17.4);
-insert into previsao (umidade, dia_Semana, descr, temp_Max, temp_Min) values (67, 'Segunda', 'Previsão 2', 23.7, 15.9);
-insert into previsao (umidade, dia_Semana, descr, temp_Max, temp_Min) values (80, 'Terça', 'Previsão 3', 25, 16.6);
-insert into previsao (umidade, dia_Semana, descr, temp_Max, temp_Min) values (72, 'Quarta', 'Previsão 4', 25.1, 17.5);
+insert into previsao (id, umidade, dia_Semana, descr, temp_Max, temp_Min) values (1, 82, 'Domingo', 'Previsão 1', 22.7, 17.4);
+insert into previsao (id, umidade, dia_Semana, descr, temp_Max, temp_Min) values (2, 67, 'Segunda', 'Previsão 2', 23.7, 15.9);
+insert into previsao (id, umidade, dia_Semana, descr, temp_Max, temp_Min) values (3, 80, 'Terça', 'Previsão 3', 25, 16.6);
+insert into previsao (id, umidade, dia_Semana, descr, temp_Max, temp_Min) values (4, 72, 'Quarta', 'Previsão 4', 25.1, 17.5);
